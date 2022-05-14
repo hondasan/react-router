ebd6c1edbba6478d3b8181bdb1aef0ecb4e29ddc
hondasan <86158971+hondasan@users.noreply.github.com>
2022-05-14 23:17:13 +0900

    クエリパラーメータ

--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,11 +1,14 @@
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 
 export const UrlParameter = () => {
   const { id } = useParams();
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
   return (
     <div>
       <h1>UrlParameter.jsxページです</h1>
       <p>パラメータはパラメータは{id}です</p>
+      <p>クエリパラメータは{query.get("name")}です</p>
     </div>
   );
 };
